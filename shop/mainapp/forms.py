f6b499d91e5afc18159793fd21e9b1f2706e4739
Plohospall <edwardpazynets@gmail.com>
2021-03-11 11:23:06 +0200

    shop

---
+++ shop/mainapp/forms.py
@@ -0,0 +1,86 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['order_date'].label = 'Date of receipt of the order'
+
+    order_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = Order
+        fields = (
+            'first_name', 'last_name', 'phone', 'address', 'buying_type', 'order_date', 'comment'
+        )
+
+class LoginForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['username'].label = 'Login'
+            self.fields['password'].label = 'Password'
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        password = self.cleaned_data['password']
+        if not User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'User with login {username} not found.')
+        user = User.objects.filter(username=username).first()
+        if user:
+            if not user.check_password(password):
+                raise forms.ValidationError('Invalid password :(')
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class RegistrationForm(forms.ModelForm):
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    phone = forms.CharField(required=False)
+    address = forms.CharField(required=False)
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Login'
+        self.fields['password'].label = 'Password'
+        self.fields['confirm_password'].label = 'Confirm the password'
+        self.fields['phone'].label = 'Phone number'
+        self.fields['first_name'].label = 'First name'
+        self.fields['last_name'].label = 'Last name'
+        self.fields['address'].label = 'Address'
+        self.fields['email'].label = 'Email'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(f'This email is already registered')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'Name {username} is already taken')
+        return username
+
+    def clean(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError(f'Password mismatch')
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'confirm_password', 'first_name', 'last_name', 'address', 'phone', 'email']
+
